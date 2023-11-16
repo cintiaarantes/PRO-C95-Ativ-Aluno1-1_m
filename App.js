e8ca9e4b91e35cd3b95c2fa7df434fe1ac02f969
Cíntia Arantes <114604292+cintiaarantes@users.noreply.github.com>
2023-11-16 11:42:50 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabNavigator from './navigation/TabNavigator';
+import DrawerNavigator from './navigation/DrawerNavigator';
+
+//Desafio 03: Chamar a navegação em gavetas
+export default function App() {
+  return (
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  );
+}
---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,3 @@
+//Desafio 02: Criar uma navegação em gavetas (adicionaremos ele na nossa tela inicial)
+//Deve-se usar o TabNavigation como uma tela em nossa navegação de gavetas
+
